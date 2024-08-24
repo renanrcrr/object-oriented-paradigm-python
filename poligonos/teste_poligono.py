4ba6a45184872139c374a3ce0beff449b925a998
renanrcrr <renanrcrr.developer@gmail.com>
2024-08-24 19:15:24 +0930

    Moving poligonos to folder.

---
+++ poligonos/teste_poligono.py
@@ -0,0 +1,10 @@
+import poligonos.poligono as poligono
+# from poligono import Quadrado
+# from poligono import * (menos recomendado)
+
+q = poligono.Quadrado(6)
+print('Area do quadrado: ' , q.area())
+print('Perimetro do quadrado: ' , q.perimetro())
+
+r = poligono.Retangulo(9, 8)
+print("Area do Retangulo: " , r.area())
